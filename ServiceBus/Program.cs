f16d49c417a3aed8c053e34117f7781d7051a04e
Mircea Pavileanu <mircea.pavileanu@solera.com>
2023-09-25 00:29:55 +0300

    Add project files.

---
+++ ServiceBus/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "Endpoint=sb://servicebus15039.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gJkcqdlj4QcVBO3gSH+SX82p38hhyQqqQ+ASbGgG6Xw=";
+
+// name of your Service Bus topic
+string queueName = "servicebus-queue";
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the sender used to publish messages to the queue
+ServiceBusSender sender;
+
+// Create the clients that we'll use for sending and processing messages.
+client = new ServiceBusClient(connectionString);
+sender = client.CreateSender(queueName);
+
+// create a batch 
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+int numberOfMessages = 100;
+
+for (int i = 1; i <= numberOfMessages; i++)
+{
+    // try adding a message to the batch
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        // if an exception occurs
+        throw new Exception($"Exception {i} has occurred.");
+    }
+}
+
+try
+{
+    // Use the producer client to send the batch of messages to the Service Bus queue
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of three messages has been published to the queue.");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Follow the directions in the exercise to review the results in the Azure portal.");
+Console.WriteLine("Press any key to continue");
+Console.ReadKey();
